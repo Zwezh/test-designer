eda2f8e0a72ac3c58a8ad31562375afb1be5593c
Zwezh <alex.zwezh@gmail.com>
2021-10-10 00:03:52 +0200

    #9 Test page
    *Package updates before angular update

--- src/test.ts
+++ src/test.ts
@@ -6,7 +6,7 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
-import 'zone.js/dist/zone-testing';
+import 'zone.js/testing';
 
 declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
